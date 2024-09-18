f8503cba7550f7a457bb9d8ec049377c5d4fd58d
vabarca <vabarca@power-electronics.com>
2024-09-18 19:22:09 +0200

    Add keyboard inputs

--- src/inputs.rs
+++ src/inputs.rs
@@ -3,5 +3,24 @@ use bevy::app::AppExit;
 
 
 pub fn quit_game(mut exit: EventWriter<AppExit>){
+    info!("Quitting game ...");
     exit.send(AppExit::Success);
 }
+
+pub fn move_player_up(){
+    info!("Move player UP");
+}
+
+pub fn move_player_down(){
+    info!("Move player DOWN");
+}
+
+pub fn move_player_left(){
+    info!("Move player LEFT");
+}
+
+pub fn move_player_right(){
+    info!("Move player RIGHT");
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
                                                 transform: Transform::from_xyz(25.0, 50.0, 0.0).with_scale(Vec3::new(0.1,0.1,0.1)),
                                                 ..Default::default()}));
 }
+
 fn _update(query: Query<&SpacecraftName, With<Player>>) {
     for name in &query {
         println!("hola {}", name.0);
@@ -28,6 +29,10 @@ fn main() {
         .add_systems(Update, 
             (
                 quit_game.run_if(input_just_pressed(KeyCode::KeyQ)),
+                move_player_up.run_if(input_just_pressed(KeyCode::ArrowUp)),
+                move_player_down.run_if(input_just_pressed(KeyCode::ArrowDown)),
+                move_player_left.run_if(input_just_pressed(KeyCode::ArrowLeft)),
+                move_player_right.run_if(input_just_pressed(KeyCode::ArrowRight)),
             ))
         .run();
 }
