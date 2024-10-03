4c43df6e3e2f13049b5ed810c1854edc1dad26f6
vabarca <vabarca@power-electronics.com>
2024-10-03 19:32:22 +0200

    Simplify vectors to 2d vector

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,14 +1,10 @@
+use bevy::{a11y::accesskit::Vec2, math::Vec3};
 use rand::prelude::*;
 
 pub fn f64_to_f32(x: f64) -> f32 {
     let y = x as f32;
     assert_eq!(x.is_finite(), y.is_finite(),);
     y
-}
-
-pub struct V2F32 {
-    pub x: f32,
-    pub y: f32,
 }
 
 pub struct V2 {
@@ -70,8 +66,18 @@ impl V2 {
         self.div(self.abs())
     }
 
-    pub fn as_v3(&self) -> V3 {
-        V3::new(self.x, self.y, 0.0)
+    pub fn as_vec3(&self) -> Vec3 {
+        Vec3::new(f64_to_f32(self.x), f64_to_f32(self.y), 0.0)
+    }
+
+    pub fn as_vec2(&self) -> Vec2 {
+        Vec2::new(self.x, self.y)
+    }
+
+    pub fn update(&mut self, v: &Vec2) -> &mut Self {
+        self.x = v.x;
+        self.y = v.y;
+        self
     }
 
     pub fn limit(&mut self, max: f64) -> &mut Self {
@@ -82,116 +88,21 @@ impl V2 {
     }
 }
 
-pub struct V3F32 {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
-}
 
-pub struct V3 {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
-}
+#[cfg(test)]
+mod tests{
 
-impl V3 {
-    pub fn new(x: f64, y: f64, z: f64) -> V3 {
-        V3 { x, y, z }
+    use super::*;
+
+    #[test]
+    fn v2_abs() {
+        let vector = V2::new(4.0, 3.0);
+        assert_eq!(vector.abs(), 5.0);
     }
 
-    pub fn zeros() -> V3 {
-        V3 {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
-        }
+    #[test]
+    fn v2_normalize() {
+        let mut vector = V2::new(4.0, 3.0);
+        assert_eq!(vector.normalize().abs(), 1.0);
     }
-
-    pub fn ones() -> V3 {
-        V3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        }
-    }
-
-    pub fn random() -> V3 {
-        V3 {
-            x: rand::thread_rng().gen(),
-            y: rand::thread_rng().gen(),
-            z: rand::thread_rng().gen(),
-        }
-    }
-
-    pub fn abs(&self) -> f64 {
-        (self.x.powf(2_f64) + self.y.powf(2_f64)).sqrt()
-    }
-
-    pub fn dot(&mut self, v: &V3) -> f64 {
-        (self.x * v.x) + (self.y * v.y) + (self.z * v.z)
-    }
-
-    pub fn add(&mut self, v: &V3) -> &mut Self {
-        self.x += v.x;
-        self.y += v.y;
-        self.z += v.z;
-        self
-    }
-
-    pub fn sub(&mut self, v: &V3) -> &mut Self {
-        self.x -= v.x;
-        self.y -= v.y;
-        self.z -= v.z;
-        self
-    }
-
-    pub fn div(&mut self, value: f64) -> &mut Self {
-        self.mul(1_f64 / value)
-    }
-
-    pub fn mul(&mut self, value: f64) -> &mut Self {
-        self.x *= value;
-        self.y *= value;
-        self.z *= value;
-        self
-    }
-
-    pub fn normalize(&mut self) -> &mut Self {
-        self.div(self.abs())
-    }
-
-    pub fn as_v2(&self) -> V2 {
-        V2::new(self.x, self.y)
-    }
-
-    pub fn limit(&mut self, max: f64) -> &mut Self {
-        if self.abs() > max {
-            self.normalize().mul(max);
-        }
-        self
-    }
-}
-
-#[test]
-fn v3_abs() {
-    let vector = V3::new(4.0, 3.0, 0.0);
-    assert_eq!(vector.abs(), 5.0);
-}
-
-#[test]
-fn v3_normalize() {
-    let mut vector = V3::new(4.0, 3.0, 0.0);
-    assert_eq!(vector.normalize().abs(), 1.0);
-}
-
-#[test]
-fn v2_abs() {
-    let vector = V2::new(4.0, 3.0);
-    assert_eq!(vector.abs(), 5.0);
-}
-
-#[test]
-fn v2_normalize() {
-    let mut vector = V2::new(4.0, 3.0);
-    assert_eq!(vector.normalize().abs(), 1.0);
 }
