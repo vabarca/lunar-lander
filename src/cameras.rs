4ee4ba9e44928eda137b392e8494301cea2665fe
vabarca <vabarca@power-electronics.com>
2024-10-31 07:57:17 +0100

    Add window resize option

--- src/cameras.rs
+++ src/cameras.rs
@@ -1,4 +1,76 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowResized};
 
 #[derive(Component)]
 pub struct CustomCamera;
+
+/// Marker component for the text that displays the current resolution.
+#[derive(Component)]
+pub struct ResolutionText;
+
+/// Stores the various window-resolutions we can select between.
+#[derive(Resource)]
+pub struct ResolutionSettings {
+    pub large: Vec2,
+    pub medium: Vec2,
+    pub small: Vec2,
+}
+
+// Spawns the UI
+pub fn setup_ui(mut cmd: Commands) {
+    // Node that fills entire background
+    cmd.spawn(NodeBundle {
+        style: Style {
+            width: Val::Percent(100.),
+            ..default()
+        },
+        ..default()
+    })
+    .with_children(|root| {
+        // Text where we display current resolution
+        root.spawn((
+            TextBundle::from_section(
+                "Resolution",
+                TextStyle {
+                    font_size: 50.0,
+                    ..default()
+                },
+            ),
+            ResolutionText,
+        ));
+    });
+}
+
+/// This system shows how to request the window to a new resolution
+pub fn toggle_resolution(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut windows: Query<&mut Window>,
+    resolution: Res<ResolutionSettings>,
+) {
+    let mut window = windows.single_mut();
+
+    if keys.just_pressed(KeyCode::Digit1) {
+        let res = resolution.small;
+        window.resolution.set(res.x, res.y);
+    }
+    if keys.just_pressed(KeyCode::Digit2) {
+        let res = resolution.medium;
+        window.resolution.set(res.x, res.y);
+    }
+    if keys.just_pressed(KeyCode::Digit3) {
+        let res = resolution.large;
+        window.resolution.set(res.x, res.y);
+    }
+}
+
+/// This system shows how to respond to a window being resized.
+/// Whenever the window is resized, the text will update with the new resolution.
+pub fn on_resize(
+    mut q: Query<&mut Text, With<ResolutionText>>,
+    mut resize_reader: EventReader<WindowResized>,
+) {
+    let mut text = q.single_mut();
+    for e in resize_reader.read() {
+        // When resolution is being changed
+        text.sections[0].value = format!("{:.1} x {:.1}", e.width, e.height);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
-use bevy::{input::common_conditions::input_just_pressed, prelude::*, render::camera::ScalingMode};
+use bevy::{input::common_conditions::input_just_pressed, prelude::*};
 use bevy_vector_shapes::prelude::*;
 use lunar_lander::{cameras::*, forces::*, inputs::*, mover::*, vectors::*};
 use rand::prelude::*;
 
-fn setup(mut commands: Commands, mut windows: Query<&mut Window>) {
-    let mut custom_camera = Camera2dBundle::default();
+fn setup(mut cmd: Commands, mut windows: Query<&mut Window>) {
 
-    custom_camera.projection.scaling_mode = ScalingMode::FixedVertical(1280.0);
+    let mut custom_camera = Camera2dBundle::default();
     custom_camera.transform = Transform::from_xyz(0.0, 0.0, 0.0);
 
     let mut window = windows.single_mut();
     window.resizable = false;
     let screen = window.resolution.physical_size().as_vec2();
-    let pos1 = V2::new((100 ) as f64, 0.0);
-    info!("{}", screen.x);
 
-    commands.spawn((custom_camera, CustomCamera));
-    
-    commands.spawn((Player, Mover::new(pos1.clone(), 40_f64)));
+    cmd.spawn((custom_camera, CustomCamera));
+    cmd.spawn((Player, Mover::new(V2::new((100 ) as f64, 0.0), 40_f64)));
     let mut rng = rand::thread_rng();
     for _ in 1..200 {
         let mut y: f64 = rng.gen();
@@ -26,7 +22,7 @@ fn setup(mut commands: Commands, mut windows: Query<&mut Window>) {
         x = x * screen.x as f64;
         let mut mass: f64 = rng.gen(); 
         mass = mass * 10f64;
-        commands.spawn((Ufo, Mover::new(V2::new(x, y), mass)));
+        cmd.spawn((Ufo, Mover::new(V2::new(x, y), mass)));
     }
 }
 
@@ -56,15 +52,22 @@ fn update(
 
 fn main() {
     App::new()
+        .insert_resource(ResolutionSettings {
+            large: Vec2::new(1920.0, 1080.0),
+            medium: Vec2::new(800.0, 600.0),
+            small: Vec2::new(640.0, 360.0),
+        })
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(Shape2dPlugin::default())
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, setup_ui))
         .add_systems(
             Update,
             (
                 quit_game.run_if(input_just_pressed(KeyCode::KeyQ)),
                 keyboard_input_system,
                 mouse_input_system,
+                toggle_resolution,
+                on_resize,
                 update,
             ),
         )
